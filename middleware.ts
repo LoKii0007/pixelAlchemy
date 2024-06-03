ca8e45d49a79a1b98d3cbbd25fb95555956fef18
Lokii0007 <lokeshyadav1098@gmail.com>
2024-06-04 02:06:16 +0530

    clerk

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default authMiddleware({
+    publicRoutes : ['/api/webhooks/clerk']
+})
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
